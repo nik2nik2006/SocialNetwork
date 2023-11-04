0fe23c2d5aab3448d18fa3bc8d4549edc2b48180
никола белоусько <belnv@yandex.ru>
2023-11-04 13:24:31 +0300

    step 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <header className='header'>
+          <img
+              src='https://w7.pngwing.com/pngs/219/256/png-transparent-phoenix-logo-flame-football-s-daquan-leaf-flower-symbol.png'/>
+      </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://w.forfun.com/fetch/1b/1bad518cb3c774425a453b16f6e9ce92.jpeg'/>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
