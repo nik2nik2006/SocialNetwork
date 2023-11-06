feeae6666571187500dfdbed8d97e1bc6b9e3443
никола белоусько <belnv@yandex.ru>
2023-11-06 18:57:37 +0300

    step 29

--- src/index.js
+++ src/index.js
@@ -3,33 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let dialogs = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Nikolay'},
-    {id: 3, name: 'Sergey'},
-    {id: 4, name: 'Misha'},
-    {id: 5, name: 'Volodya'},
-    {id: 6, name: 'Valera'},
-];
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'Its your frend'},
-    {id: 3, message: 'Hi'},
-    {id: 4, message: 'Hi'},
-    {id: 5, message: 'Hi'},
-];
-let posts = [
-    {id: 1, message: 'Yo', likeCount: '5'},
-    {id: 2, message: 'Yoklmn', likeCount: '4'},
-    {id: 3, message: 'Yoprst', likeCount: '23'},
-    {id: 4, message: 'Yoxoxo', likeCount: '0'},
-    {id: 5, message: 'Yoshkin kot', likeCount: '7'},
-];
+
 root.render(
   <React.StrictMode>
-    <App dialogs={dialogs} messages={messages} posts={posts}/>
+    <App state={state} />
   </React.StrictMode>
 );
 
---
+++ src/redux/state.js
@@ -0,0 +1,30 @@
+let state = {
+    profilePage: {
+        posts: [
+        {id: 1, message: 'Yo', likeCount: '5'},
+        {id: 2, message: 'Yoklmn', likeCount: '4'},
+        {id: 3, message: 'Yoprst', likeCount: '23'},
+        {id: 4, message: 'Yoxoxo', likeCount: '0'},
+        {id: 5, message: 'Yoshkin kot', likeCount: '7'},
+    ],
+    },
+    dialogPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Its your frend'},
+            {id: 3, message: 'Hi'},
+            {id: 4, message: 'Hi'},
+            {id: 5, message: 'Hi'},
+        ],
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Nikolay'},
+            {id: 3, name: 'Sergey'},
+            {id: 4, name: 'Misha'},
+            {id: 5, name: 'Volodya'},
+            {id: 6, name: 'Valera'},
+        ],
+    }
+}
+
+export default state;
