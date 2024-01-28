91ba19353952225f21fb872cc749573d8c55cec5
nik2nik2006 <belnv@yandex.ru>
2024-01-28 14:37:33 +0300

    step 94

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/login";
 import {connect, Provider} from "react-redux";
@@ -17,29 +15,35 @@ import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./redux/redux-store";
 
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+
 
 class App extends React.Component {
     componentDidMount() {
         this.props.initializeApp();
     }
+
     render() {
         if (!this.props.initialized) {
-            return  <Preloader />
+            return <Preloader/>
         }
         return (
             <div className='app-wrapper'>
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
-                        <Route path='/users' element={<UsersContainer/>}/>
-                        <Route path='/dialogs/*' element={< DialogsContainer/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-                        <Route path='/login' element={<LoginPage/>}/>
-                    </Routes>
+                    <React.Suspense fallback={Preloader}>
+                        <Routes>
+                            <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                            <Route path='/users' element={<UsersContainer/>}/>
+                            <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='/music' element={<Music/>}/>
+                            <Route path='/settings' element={<Settings/>}/>
+                            <Route path='/login' element={<LoginPage/>}/>
+                        </Routes>
+                    </React.Suspense>
                 </div>
             </div>
         );
@@ -50,7 +54,7 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-const AppContainer = compose(withRouter, connect(mapStateToProps, {initializeApp})) (App);
+const AppContainer = compose(withRouter, connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJSApp = (props) => {
     return <BrowserRouter>
--- src/components/Common/Paginator/Paginator.jsx
+++ src/components/Common/Paginator/Paginator.jsx
@@ -3,8 +3,7 @@ import styles from "./Paginator.module.css";
 import cn from "classnames";
 
 
-let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize=5}) => {
-    debugger
+let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 5}) => {
     let pagesCount = Math.ceil(totalItemsCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
@@ -17,22 +16,26 @@ let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portion
     let rightPortionPageNumber = portionNumber * portionSize;
 
     return <div className={styles.paginator}>
-        { portionNumber > 1 &&
-            <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button> }
+        {portionNumber > 1 &&
+            <button className={styles.button} onClick={() => {
+                setPortionNumber(portionNumber - 1)
+            }}>PREV</button>}
 
         {pages
-            .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
             .map((p) => {
-                return <span className={ cn ({
+                return <span className={cn({
                     [styles.selectedPage]: currentPage === p
-                }, styles.pageNumber) }
+                }, styles.pageNumber)}
                              key={p}
                              onClick={(e) => {
                                  onPageChanged(p);
                              }}> {p} </span>
             })}
-        { portionCount > portionNumber &&
-            <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button> }
+        {portionCount > portionNumber &&
+            <button className={styles.button} onClick={() => {
+                setPortionNumber(portionNumber + 1)
+            }}>NEXT</button>}
 
 
     </div>
